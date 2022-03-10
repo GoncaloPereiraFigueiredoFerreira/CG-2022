a2471ae0f83e89c8de596c232c46c9528a4e8269
Alex-Sa-Ms <56095867+Alex-Sa-Ms@users.noreply.github.com>
2022-03-10 21:20:34 +0000

    Eliminado ficheiros desnecessarios

--- Generator/SolidGenerator/SolidGenerator/BoxGenerator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Box.h"
-#include <string>
-#include <vector>
-#include <fstream>
-
-Box generateBox(float lenght, int div);
-void generateBoxFile(std::string filename, Box box);
-Box* readBoxFromFile(std::ifstream& fd);
--- generator.cpp
+++
@@ -1,7 +0,0 @@
-// This file will contain the code for the generation of 3d models
-
-//Here we will store the vertice's information, in order to draw it on the engine
-
-
-
-
