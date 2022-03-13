cfb6584dc05ca41e105cf3d635b7324cf88dc960
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-03-13 01:44:48 +0000

    change box orientain

--- Engine3D/Models/Box.cpp
+++ Engine3D/Models/Box.cpp
@@ -9,64 +9,64 @@ void Box::draw() {
 
 			if (i && j) {
 				///under
-				glVertex3f(mat_u[i - 1][j].cx, mat_u[i - 1][j].cy, mat_u[i - 1][j].cz);
+				glVertex3f(mat_u[i - 1][j - 1].cx, mat_u[i - 1][j - 1].cy, mat_u[i - 1][j - 1].cz);
 				glVertex3f(mat_u[i][j].cx, mat_u[i][j].cy, mat_u[i][j].cz);
 				glVertex3f(mat_u[i][j - 1].cx, mat_u[i][j - 1].cy, mat_u[i][j - 1].cz);
 
-				glVertex3f(mat_u[i - 1][j].cx, mat_u[i - 1][j].cy, mat_u[i - 1][j].cz);
-				glVertex3f(mat_u[i][j - 1].cx, mat_u[i][j - 1].cy, mat_u[i][j - 1].cz);
 				glVertex3f(mat_u[i - 1][j - 1].cx, mat_u[i - 1][j - 1].cy, mat_u[i - 1][j - 1].cz);
+				glVertex3f(mat_u[i - 1][j].cx, mat_u[i - 1][j].cy, mat_u[i - 1][j].cz);
+				glVertex3f(mat_u[i][j].cx, mat_u[i][j].cy, mat_u[i][j].cz);
 				///
 
 				///Right
-				glVertex3f(mat_r[i - 1][j].cx, mat_r[i - 1][j].cy, mat_r[i - 1][j].cz);
+				glVertex3f(mat_r[i - 1][j - 1].cx, mat_r[i - 1][j - 1].cy, mat_r[i - 1][j - 1].cz);
 				glVertex3f(mat_r[i][j - 1].cx, mat_r[i][j - 1].cy, mat_r[i][j - 1].cz);
 				glVertex3f(mat_r[i][j].cx, mat_r[i][j].cy, mat_r[i][j].cz);
 
-				glVertex3f(mat_r[i - 1][j].cx, mat_r[i - 1][j].cy, mat_r[i - 1][j].cz);
 				glVertex3f(mat_r[i - 1][j - 1].cx, mat_r[i - 1][j - 1].cy, mat_r[i - 1][j - 1].cz);
-				glVertex3f(mat_r[i][j - 1].cx, mat_r[i][j - 1].cy, mat_r[i][j - 1].cz);
+				glVertex3f(mat_r[i][j].cx, mat_r[i][j].cy, mat_r[i][j].cz);
+				glVertex3f(mat_r[i - 1][j].cx, mat_r[i - 1][j].cy, mat_r[i - 1][j].cz);
 				///
 
 				///front
-				glVertex3f(mat_f[i - 1][j].cx, mat_f[i - 1][j].cy, mat_f[i - 1][j].cz);
+				glVertex3f(mat_f[i - 1][j - 1].cx, mat_f[i - 1][j - 1].cy, mat_f[i - 1][j - 1].cz);
 				glVertex3f(mat_f[i][j - 1].cx, mat_f[i][j - 1].cy, mat_f[i][j - 1].cz);
 				glVertex3f(mat_f[i][j].cx, mat_f[i][j].cy, mat_f[i][j].cz);
 
-				glVertex3f(mat_f[i - 1][j].cx, mat_f[i - 1][j].cy, mat_f[i - 1][j].cz);
 				glVertex3f(mat_f[i - 1][j - 1].cx, mat_f[i - 1][j - 1].cy, mat_f[i - 1][j - 1].cz);
-				glVertex3f(mat_f[i][j - 1].cx, mat_f[i][j - 1].cy, mat_f[i][j - 1].cz);
+				glVertex3f(mat_f[i][j].cx, mat_f[i][j].cy, mat_f[i][j].cz);
+				glVertex3f(mat_f[i - 1][j].cx, mat_f[i - 1][j].cy, mat_f[i - 1][j].cz);
 				///
 
 
 				///Top
-				glVertex3f(mat_t[i - 1][j].cx, mat_t[i - 1][j].cy, mat_t[i - 1][j].cz);
+				glVertex3f(mat_t[i - 1][j - 1].cx, mat_t[i - 1][j - 1].cy, mat_t[i - 1][j - 1].cz);
 				glVertex3f(mat_t[i][j - 1].cx, mat_t[i][j - 1].cy, mat_t[i][j - 1].cz);
 				glVertex3f(mat_t[i][j].cx, mat_t[i][j].cy, mat_t[i][j].cz);
 
-				glVertex3f(mat_t[i - 1][j].cx, mat_t[i - 1][j].cy, mat_t[i - 1][j].cz);
 				glVertex3f(mat_t[i - 1][j - 1].cx, mat_t[i - 1][j - 1].cy, mat_t[i - 1][j - 1].cz);
-				glVertex3f(mat_t[i][j - 1].cx, mat_t[i][j - 1].cy, mat_t[i][j - 1].cz);
+				glVertex3f(mat_t[i][j].cx, mat_t[i][j].cy, mat_t[i][j].cz);
+				glVertex3f(mat_t[i - 1][j].cx, mat_t[i - 1][j].cy, mat_t[i - 1][j].cz);
 				///
 
 				///Left
-				glVertex3f(mat_l[i - 1][j].cx, mat_l[i - 1][j].cy, mat_l[i - 1][j].cz);
+				glVertex3f(mat_l[i - 1][j - 1].cx, mat_l[i - 1][j - 1].cy, mat_l[i - 1][j - 1].cz);
 				glVertex3f(mat_l[i][j].cx, mat_l[i][j].cy, mat_l[i][j].cz);
 				glVertex3f(mat_l[i][j - 1].cx, mat_l[i][j - 1].cy, mat_l[i][j - 1].cz);
 
-				glVertex3f(mat_l[i - 1][j].cx, mat_l[i - 1][j].cy, mat_l[i - 1][j].cz);
-				glVertex3f(mat_l[i][j - 1].cx, mat_l[i][j - 1].cy, mat_l[i][j - 1].cz);
 				glVertex3f(mat_l[i - 1][j - 1].cx, mat_l[i - 1][j - 1].cy, mat_l[i - 1][j - 1].cz);
+				glVertex3f(mat_l[i - 1][j].cx, mat_l[i - 1][j].cy, mat_l[i - 1][j].cz);
+				glVertex3f(mat_l[i][j].cx, mat_l[i][j].cy, mat_l[i][j].cz);
 				///
 
 				///Back
-				glVertex3f(mat_b[i - 1][j].cx, mat_b[i - 1][j].cy, mat_b[i - 1][j].cz);
+				glVertex3f(mat_b[i - 1][j - 1].cx, mat_b[i - 1][j - 1].cy, mat_b[i - 1][j - 1].cz);
 				glVertex3f(mat_b[i][j].cx, mat_b[i][j].cy, mat_b[i][j].cz);
 				glVertex3f(mat_b[i][j - 1].cx, mat_b[i][j - 1].cy, mat_b[i][j - 1].cz);
 
-				glVertex3f(mat_b[i - 1][j].cx, mat_b[i - 1][j].cy, mat_b[i - 1][j].cz);
-				glVertex3f(mat_b[i][j - 1].cx, mat_b[i][j - 1].cy, mat_b[i][j - 1].cz);
 				glVertex3f(mat_b[i - 1][j - 1].cx, mat_b[i - 1][j - 1].cy, mat_b[i - 1][j - 1].cz);
+				glVertex3f(mat_b[i - 1][j].cx, mat_b[i - 1][j].cy, mat_b[i - 1][j].cz);
+				glVertex3f(mat_b[i][j].cx, mat_b[i][j].cy, mat_b[i][j].cz);
 				///
 			}
 		}
