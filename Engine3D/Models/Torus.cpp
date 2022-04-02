44719debe39f2da3bab7891e8a5bae40386a9758
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-04-02 19:29:02 +0100

    correçao stacks to stacks torus

--- Engine3D/Models/Torus.cpp
+++ Engine3D/Models/Torus.cpp
@@ -3,8 +3,8 @@
 void Torus::draw() {
 	glBegin(GL_TRIANGLES);
 
-	for(int j = 0; j < slices;j++){
-		for(int i = 0; i < stacks;i++){
+	for(int i = 0; i < slices;i++){
+		for(int j = 0; j < stacks;j++){
 			glVertex3f(mat[j][i].cx,mat[j][i].cy,mat[j][i].cz);
 			glVertex3f(mat[(j + 1)%stacks][i].cx,mat[(j + 1)%stacks][i].cy,mat[(j + 1)%stacks][i].cz);
 			glVertex3f(mat[j][(i + 1)%slices].cx,mat[j][(i + 1)%slices].cy,mat[j][(i + 1)%slices].cz);
