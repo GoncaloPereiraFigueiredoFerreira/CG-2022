2c99afb64a3794292da1235baa86f3b9af80d76c
Alex-Sa-Ms <a93315@alunos.uminho.pt>
2022-04-13 23:39:57 +0100

    Adicionada funcao que preenche vector de vértices e vector de indices de uma esfera

--- Engine3D/Generator/SphereGenerator.cpp
+++ Engine3D/Generator/SphereGenerator.cpp
@@ -2,6 +2,94 @@
 
 using namespace std;
 
+void sphere(float radius, int slices, int stacks, vector<float> vvector, vector<unsigned int> ivector) {
+    //Inicializacao do vector onde serao guardados os pontos
+    unsigned int verticeCount = (stacks - 1) * slices + 2;
+    vvector.resize(verticeCount * 3);
+
+    //Preenchimento do vector
+    int indexLowestP = ((int) verticeCount - 2) * 3;
+    vvector[indexLowestP] = 0;
+    vvector[indexLowestP + 1] = -radius;
+    vvector[indexLowestP + 2] = 0;
+
+    int indexHighestP = ((int) verticeCount - 1) * 3;
+    vvector[indexHighestP] = 0;
+    vvector[indexHighestP + 1] = radius;
+    vvector[indexHighestP + 2] = 0;
+
+    float stacksAngInc = M_PI / stacks;
+    float slicesAngInc = 2 * M_PI / slices; //Valor do angulo a aumentar entre cada ponto que define uma slice
+    float ang, height, r;
+
+    for (int i = 0; i < stacks - 1; i++) {
+        height = radius * sin(-M_PI_2 + stacksAngInc * (float) (i + 1));
+        r = radius * cos(-M_PI_2 + stacksAngInc * (float) (i + 1)); //raio da "circunferencia" atual
+
+        //Preenche valores da stack com indice i
+        for (int j = 0; j < slices; j++) {
+            ang = slicesAngInc * j;
+            int index = (i * slices + j) * 3;
+            vvector[index] = r * sin(ang);
+            vvector[index + 1] = height;
+            vvector[index + 2] = r * cos(ang);
+        }
+    }
+
+    //Draws the upper and lower stacks
+    for (int j = 0; j < slices - 1; j++) {
+        ivector.push_back(j);
+        ivector.push_back(verticeCount - 2);
+        ivector.push_back(j + 1);
+
+        ivector.push_back(verticeCount - 1);
+        ivector.push_back((stacks - 2) * slices + j);
+        ivector.push_back((stacks - 2) * slices + j + 1);
+    }
+
+    //Desenha triangulos correspondentes � slice que completa uma volta
+    ivector.push_back(slices - 1);
+    ivector.push_back(verticeCount - 2);
+    ivector.push_back(0);
+
+    ivector.push_back(verticeCount - 1);
+    ivector.push_back((stacks - 2) * slices + (slices - 1));
+    ivector.push_back((stacks - 2) * slices);
+
+
+    //Desenho das restantes stacks
+
+    for (int i = 0; i < stacks - 2; i++) {
+
+        for (int j = 0; j < slices - 1; j++) {
+
+            /*    Desenha os triangulos com forma |\    */
+            ivector.push_back(i * slices + j);
+            ivector.push_back(i * slices + j + 1);
+            ivector.push_back((i + 1) * slices + j);
+
+            /*     Desenha os triangulos com forma \|    */
+            ivector.push_back(i * slices + j + 1);
+            ivector.push_back((i + 1) * slices + j + 1);
+            ivector.push_back((i + 1) * slices + j);
+
+        }
+
+
+        //Desenha triangulos correspondentes � slice que completa uma volta
+
+        /*    Desenha os triangulos com forma |\    */
+        ivector.push_back(i * slices + slices - 1);
+        ivector.push_back(i * slices);
+        ivector.push_back((i + 1) * slices + slices - 1);
+
+        /*     Desenha os triangulos com forma \|    */
+        ivector.push_back(i * slices);
+        ivector.push_back((i + 1) * slices);
+        ivector.push_back((i + 1) * slices + slices - 1);
+    }
+}
+
 void generateSphereFile(string filename, Sphere sphere) {
 	ofstream fich;
 	fich.open(filename, ios::out);
